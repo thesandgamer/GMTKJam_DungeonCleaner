0a8d26e69c1250468d1fb6bff8c59d3d1c7c367a
thesandgamer <36230113+thesandgamer@users.noreply.github.com>
2023-07-09 17:21:43 +0200

    fixes

--- Assets/2 Scripts/Character/Scr_SwitchForm.cs
+++ Assets/2 Scripts/Character/Scr_SwitchForm.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 
@@ -25,8 +24,8 @@ public class Scr_SwitchForm : MonoBehaviour
     [SerializeField] private Sprite bigHand;
     [SerializeField] private Sprite smallHand;
     
-    [SerializeField] private AnimatorController big;
-    [SerializeField] private AnimatorController small;
+    [SerializeField] private RuntimeAnimatorController  big;
+    [SerializeField] private RuntimeAnimatorController  small;
     
 
     private void Awake()
