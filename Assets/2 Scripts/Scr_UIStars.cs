caead65d08e8aafc8f568c2a2e412a726fd9319f
thesandgamer <36230113+thesandgamer@users.noreply.github.com>
2023-07-09 18:29:42 +0200

    wip

--- Assets/2 Scripts/Scr_UIStars.cs
+++ Assets/2 Scripts/Scr_UIStars.cs
@@ -5,17 +5,131 @@ using UnityEngine;
 
 public class Scr_UIStars : MonoBehaviour
 {
-    [SerializeField]  private List<Sprite> starImages;
+    [SerializeField]  private List<GameObject> starImagesHalf;
+    [SerializeField]  private List<GameObject> starFullImages;
 
+    [Range(0,5)]
+    public float score = 0;
 
     private void Start()
     {
         Calculate();
     }
 
+    private void Update()
+    {
+       // Calculate();
+    }
+
     void Calculate()
-    { 
-        float score = FindObjectOfType<Scr_Score>().score;
+    {
+        if (FindObjectOfType<Scr_Score>())
+        {   
+            score = FindObjectOfType<Scr_Score>().score;
+
+        }
+
+        if (score <= 0)
+        {
+            DeactivateAll();
+            
+        }
+        else if (score >= 0.5 && score < 1)
+        {
+            DeactivateAll();
+            starImagesHalf[0].SetActive(true);
+            
+        }
+        else if (score >= 1 && score < 1.5)
+        {
+            DeactivateAll();
+            starFullImages[0].SetActive(true);
+            
+        }
+        else if (score >= 1.5 && score < 2)
+        {
+            DeactivateAll();
+            starFullImages[0].SetActive(true);
+            starImagesHalf[1].SetActive(true);
+            
+        }
+        else if (score >= 2 && score < 2.5)
+        {
+            DeactivateAll();
+            starFullImages[0].SetActive(true);
+            starFullImages[1].SetActive(true);
+            
+        }
+        else if (score >= 2.5 && score < 3)
+        {
+            DeactivateAll();
+            starFullImages[0].SetActive(true);
+            starFullImages[1].SetActive(true);
+            starImagesHalf[2].SetActive(true);
+            
+        }
+        else if (score >= 3 && score < 3.5)
+        {
+            DeactivateAll();
+            starFullImages[0].SetActive(true);
+            starFullImages[1].SetActive(true);
+            starFullImages[2].SetActive(true);
+            
+        }
+        else if (score >= 3.5 && score < 4)
+        {
+            DeactivateAll();
+            starFullImages[0].SetActive(true);
+            starFullImages[1].SetActive(true);
+            starFullImages[2].SetActive(true);
+            starImagesHalf[3].SetActive(true);
+            
+        }
+        else if (score >= 4 && score < 4.5)
+        {
+            DeactivateAll();
+            starFullImages[0].SetActive(true);
+            starFullImages[1].SetActive(true);
+            starFullImages[2].SetActive(true);
+            starFullImages[3].SetActive(true);
+            
+        }
+        else if (score >= 4.5 && score < 5)
+        {
+            DeactivateAll();
+            starImagesHalf[0].SetActive(true);
+            starFullImages[0].SetActive(true);
+            starFullImages[1].SetActive(true);
+            starFullImages[2].SetActive(true);
+            starFullImages[3].SetActive(true);
+            starImagesHalf[4].SetActive(true);
+            
+        }
+        else if (score >= 5)
+        {
+            DeactivateAll();
+            starImagesHalf[0].SetActive(true);
+            starFullImages[0].SetActive(true);
+            starFullImages[1].SetActive(true);
+            starFullImages[2].SetActive(true);
+            starFullImages[3].SetActive(true);
+            starFullImages[4].SetActive(true);
+            
+        }
         
     }
+    
+    void DeactivateAll()
+    {
+        foreach (var star in starImagesHalf)
+        {
+            star.SetActive(false);
+        }
+        foreach (var star in starFullImages)
+        {
+            star.SetActive(false);
+
+        }
+    }
 }
+
